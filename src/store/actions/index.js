beb942a7179de73fc613a13d5eb6b718fe8114b4
Thomas Eggenberger <meshugg83@yahoo.com>
2018-08-22 19:07:33 -0600

    fetch orders with redux

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,2 @@
 export { addQuantity, removeQuantity, initQuantity } from "./tireTracker";
-export { purchaseTire, purchaseInit } from './order';
+export { purchaseTire, purchaseInit, fetchOrders } from './order';
